4fade8aa28fe835295369243a3383a38622faac6
Akash <flippoalone@gmail.com>
2026-03-15 16:29:31 +0530

    Update

---
+++ src/Array_Basic/StoreNumber.java
@@ -0,0 +1,15 @@
+package Array_Basic;
+
+public class StoreNumber {
+    public  static void main(String[] args){
+
+
+        int[] number ={1 , 2 , 3 , 4 , 5 };
+
+        // Using Enhanced for loop.
+        for (int num: number){
+            System.out.println(num);
+        }
+
+    }
+}
