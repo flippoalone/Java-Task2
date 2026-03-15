0a33bd0b72616f32506c56780aa056d211438d40
Akash <flippoalone@gmail.com>
2026-03-15 19:22:39 +0530

    Update

---
+++ src/Array_Basic/SmallestNumber.java
@@ -0,0 +1,17 @@
+package Array_Basic;
+
+public class SmallestNumber {
+    public static void main(String[] args){
+
+        int[] number = {10, 22, 30, 47,15, 68, };
+        int sum = number[0];
+
+        for (int i = 0 ; i < number.length; i++){
+
+            if(number[i] < sum){
+                sum = number[i];
+            }
+        }
+        System.out.println(sum);
+    }
+}
